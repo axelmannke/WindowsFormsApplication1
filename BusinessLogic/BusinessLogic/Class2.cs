d1288fd7c4aa68df0575c008ce8bfc8e407f1135
Mk <Mk@5f019162h>
2017-10-24 08:20:56 +0200

    Projektdateien hinzufügen.

---
+++ BusinessLogic/BusinessLogic/Class2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public abstract class A_Collection
+    {
+        public string Name;
+    }
+
+    public class M_Collection : A_Collection
+    {
+
+    }
+
+    public class M_Collection2 : A_Collection
+    {
+
+    }
+
+}
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BusinessLogic;
+
+namespace WindowsFormsApplication1
+{
+
+    
+
+    public partial class Form1 : Form
+    {
+        private Halter halter;
+        public Form1()
+        {
+            InitializeComponent();
+            
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Konto k = new Konto(3);
+            //treeView1.Nodes.Add(k.node);
+
+            //VKonto vk = new VKonto(k);
+
+            //treeView1.Nodes.Add(vk.node);
+
+            RootKonten rk = new RootKonten(treeView1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            MessageBox.Show(this.halter.Name);
+        
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void treeView1_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            TreeNode n = treeView1.SelectedNode;
+
+            try
+            {
+                textBox1.Text = ((Konto)n.Tag).Soll.ToString();
+            }
+            catch { }
+        }
+    }
+}
